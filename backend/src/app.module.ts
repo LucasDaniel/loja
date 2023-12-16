8162696dbcdc5ee1bf02c5d448a699df62b4f190
A1034512 <lucasrodrigues@educarbrasil.org.br>
2023-12-16 18:52:08 -0300

    envio de email com nestjs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,6 +9,8 @@ import { PacoteModule } from './pacote/pacote.module';
 import { CoberturaModule } from './cobertura/cobertura.module';
 import { UsuarioPacoteModule } from './usuario_pacote/usuarioPacote.module';
 import { UsuarioServicoModule } from './usuario_servico/usuarioServico.module';
+import { SendEmailModule } from './send_email/sendEmail.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
@@ -18,6 +20,16 @@ import { UsuarioServicoModule } from './usuario_servico/usuarioServico.module';
     CoberturaModule,
     UsuarioPacoteModule,
     UsuarioServicoModule,
+    SendEmailModule,
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        auth: {
+          user: 'lucasd.beltrame@gmail.com',
+          pass: 'vxqewylkvgjycabe',
+        },
+      },
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
